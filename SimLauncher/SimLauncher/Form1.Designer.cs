e3282d5cad32b252e5c7bd6e27f17df3231513e2
Bacon8tor <droidstealer4allah@gmail.com>
2022-05-13 07:01:43 -0700

    uploaded project files

---
+++ SimLauncher/SimLauncher/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿
+namespace SimLauncher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.msm = new MetroFramework.Components.MetroStyleManager(this.components);
+            this.mainFLP = new System.Windows.Forms.FlowLayoutPanel();
+            this.controlPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.metroPanel1 = new MetroFramework.Controls.MetroPanel();
+            this.addWebPage = new MetroFramework.Controls.MetroButton();
+            this.addDelay = new MetroFramework.Controls.MetroButton();
+            this.programNameTextBox = new MetroFramework.Controls.MetroTextBox();
+            this.programAdd = new MetroFramework.Controls.MetroButton();
+            this.metroPanel3 = new MetroFramework.Controls.MetroPanel();
+            this.createShortcut = new MetroFramework.Controls.MetroButton();
+            this.metroPanel2 = new MetroFramework.Controls.MetroPanel();
+            this.styleBox = new MetroFramework.Controls.MetroComboBox();
+            this.themeBox = new MetroFramework.Controls.MetroComboBox();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.prgmPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.iconAdd = new MetroFramework.Controls.MetroButton();
+            ((System.ComponentModel.ISupportInitialize)(this.msm)).BeginInit();
+            this.mainFLP.SuspendLayout();
+            this.controlPanel.SuspendLayout();
+            this.metroPanel1.SuspendLayout();
+            this.metroPanel3.SuspendLayout();
+            this.metroPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // msm
+            // 
+            this.msm.Owner = this;
+            // 
+            // mainFLP
+            // 
+            this.mainFLP.BackColor = System.Drawing.Color.Transparent;
+            this.mainFLP.Controls.Add(this.controlPanel);
+            this.mainFLP.Controls.Add(this.prgmPanel);
+            this.mainFLP.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainFLP.Location = new System.Drawing.Point(20, 60);
+            this.mainFLP.Name = "mainFLP";
+            this.mainFLP.Size = new System.Drawing.Size(748, 416);
+            this.mainFLP.TabIndex = 0;
+            // 
+            // controlPanel
+            // 
+            this.controlPanel.Controls.Add(this.metroPanel1);
+            this.controlPanel.Controls.Add(this.metroPanel3);
+            this.controlPanel.Controls.Add(this.metroPanel2);
+            this.controlPanel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.controlPanel.Location = new System.Drawing.Point(3, 3);
+            this.controlPanel.Name = "controlPanel";
+            this.controlPanel.Size = new System.Drawing.Size(200, 410);
+            this.controlPanel.TabIndex = 0;
+            // 
+            // metroPanel1
+            // 
+            this.metroPanel1.Controls.Add(this.iconAdd);
+            this.metroPanel1.Controls.Add(this.addWebPage);
+            this.metroPanel1.Controls.Add(this.addDelay);
+            this.metroPanel1.Controls.Add(this.programNameTextBox);
+            this.metroPanel1.Controls.Add(this.programAdd);
+            this.metroPanel1.HorizontalScrollbarBarColor = true;
+            this.metroPanel1.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.HorizontalScrollbarSize = 10;
+            this.metroPanel1.Location = new System.Drawing.Point(3, 3);
+            this.metroPanel1.Name = "metroPanel1";
+            this.metroPanel1.Size = new System.Drawing.Size(197, 159);
+            this.metroPanel1.TabIndex = 0;
+            this.metroPanel1.VerticalScrollbarBarColor = true;
+            this.metroPanel1.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.VerticalScrollbarSize = 10;
+            // 
+            // addWebPage
+            // 
+            this.addWebPage.Location = new System.Drawing.Point(3, 90);
+            this.addWebPage.Name = "addWebPage";
+            this.addWebPage.Size = new System.Drawing.Size(191, 23);
+            this.addWebPage.TabIndex = 5;
+            this.addWebPage.Text = "Add WebPage";
+            this.addWebPage.Click += new System.EventHandler(this.addWebPage_Click);
+            // 
+            // addDelay
+            // 
+            this.addDelay.Location = new System.Drawing.Point(3, 61);
+            this.addDelay.Name = "addDelay";
+            this.addDelay.Size = new System.Drawing.Size(191, 23);
+            this.addDelay.TabIndex = 4;
+            this.addDelay.Text = "Add Delay";
+            this.addDelay.Click += new System.EventHandler(this.addPrgmDelay);
+            // 
+            // programNameTextBox
+            // 
+            this.programNameTextBox.Location = new System.Drawing.Point(3, 3);
+            this.programNameTextBox.Name = "programNameTextBox";
+            this.programNameTextBox.Size = new System.Drawing.Size(191, 23);
+            this.programNameTextBox.TabIndex = 2;
+            this.programNameTextBox.Text = "Program Name";
+            // 
+            // programAdd
+            // 
+            this.programAdd.Location = new System.Drawing.Point(3, 32);
+            this.programAdd.Name = "programAdd";
+            this.programAdd.Size = new System.Drawing.Size(191, 23);
+            this.programAdd.TabIndex = 3;
+            this.programAdd.Text = "Add Program";
+            this.programAdd.Click += new System.EventHandler(this.addPrgm);
+            // 
+            // metroPanel3
+            // 
+            this.metroPanel3.Controls.Add(this.createShortcut);
+            this.metroPanel3.HorizontalScrollbarBarColor = true;
+            this.metroPanel3.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel3.HorizontalScrollbarSize = 10;
+            this.metroPanel3.Location = new System.Drawing.Point(3, 168);
+            this.metroPanel3.Name = "metroPanel3";
+            this.metroPanel3.Size = new System.Drawing.Size(200, 60);
+            this.metroPanel3.TabIndex = 7;
+            this.metroPanel3.VerticalScrollbarBarColor = true;
+            this.metroPanel3.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel3.VerticalScrollbarSize = 10;
+            // 
+            // createShortcut
+            // 
+            this.createShortcut.Location = new System.Drawing.Point(3, 3);
+            this.createShortcut.Name = "createShortcut";
+            this.createShortcut.Size = new System.Drawing.Size(191, 23);
+            this.createShortcut.TabIndex = 6;
+            this.createShortcut.Text = "Create Shortcut";
+            this.createShortcut.Click += new System.EventHandler(this.createShortcut_Click);
+            // 
+            // metroPanel2
+            // 
+            this.metroPanel2.Controls.Add(this.styleBox);
+            this.metroPanel2.Controls.Add(this.themeBox);
+            this.metroPanel2.Controls.Add(this.metroLabel1);
+            this.metroPanel2.HorizontalScrollbarBarColor = true;
+            this.metroPanel2.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel2.HorizontalScrollbarSize = 10;
+            this.metroPanel2.Location = new System.Drawing.Point(3, 234);
+            this.metroPanel2.Name = "metroPanel2";
+            this.metroPanel2.Size = new System.Drawing.Size(197, 126);
+            this.metroPanel2.TabIndex = 1;
+            this.metroPanel2.VerticalScrollbarBarColor = true;
+            this.metroPanel2.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel2.VerticalScrollbarSize = 10;
+            // 
+            // styleBox
+            // 
+            this.styleBox.FormattingEnabled = true;
+            this.styleBox.ItemHeight = 23;
+            this.styleBox.Location = new System.Drawing.Point(17, 58);
+            this.styleBox.Name = "styleBox";
+            this.styleBox.Size = new System.Drawing.Size(121, 29);
+            this.styleBox.TabIndex = 4;
+            this.styleBox.SelectedIndexChanged += new System.EventHandler(this.styleBox_SelectedValueChanged);
+            // 
+            // themeBox
+            // 
+            this.themeBox.FormattingEnabled = true;
+            this.themeBox.ItemHeight = 23;
+            this.themeBox.Location = new System.Drawing.Point(17, 23);
+            this.themeBox.Name = "themeBox";
+            this.themeBox.Size = new System.Drawing.Size(121, 29);
+            this.themeBox.TabIndex = 3;
+            this.themeBox.SelectedIndexChanged += new System.EventHandler(this.themeBox_SelectedIndexChanged);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(3, 0);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(49, 19);
+            this.metroLabel1.TabIndex = 2;
+            this.metroLabel1.Text = "Theme";
+            // 
+            // prgmPanel
+            // 
+            this.prgmPanel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.prgmPanel.Location = new System.Drawing.Point(209, 3);
+            this.prgmPanel.Name = "prgmPanel";
+            this.prgmPanel.Size = new System.Drawing.Size(536, 410);
+            this.prgmPanel.TabIndex = 1;
+            // 
+            // iconAdd
+            // 
+            this.iconAdd.Location = new System.Drawing.Point(3, 119);
+            this.iconAdd.Name = "iconAdd";
+            this.iconAdd.Size = new System.Drawing.Size(191, 23);
+            this.iconAdd.TabIndex = 6;
+            this.iconAdd.Text = "Add Icon";
+            this.iconAdd.Click += new System.EventHandler(this.iconAdd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(788, 496);
+            this.Controls.Add(this.mainFLP);
+            this.Name = "Form1";
+            this.Text = "SimLauncher";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.msm)).EndInit();
+            this.mainFLP.ResumeLayout(false);
+            this.controlPanel.ResumeLayout(false);
+            this.metroPanel1.ResumeLayout(false);
+            this.metroPanel3.ResumeLayout(false);
+            this.metroPanel2.ResumeLayout(false);
+            this.metroPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Components.MetroStyleManager msm;
+        private System.Windows.Forms.FlowLayoutPanel mainFLP;
+        private System.Windows.Forms.FlowLayoutPanel controlPanel;
+        private MetroFramework.Controls.MetroPanel metroPanel1;
+        private MetroFramework.Controls.MetroTextBox programNameTextBox;
+        private MetroFramework.Controls.MetroButton programAdd;
+        private MetroFramework.Controls.MetroPanel metroPanel2;
+        private MetroFramework.Controls.MetroComboBox styleBox;
+        private MetroFramework.Controls.MetroComboBox themeBox;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private System.Windows.Forms.FlowLayoutPanel prgmPanel;
+        private MetroFramework.Controls.MetroButton addWebPage;
+        private MetroFramework.Controls.MetroButton addDelay;
+        private MetroFramework.Controls.MetroPanel metroPanel3;
+        private MetroFramework.Controls.MetroButton createShortcut;
+        private MetroFramework.Controls.MetroButton iconAdd;
+    }
+}
+
---
+++ SimLauncher/SimLauncher/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework;
+using MetroFramework.Controls;
+using MetroFramework.Forms;
+using System.Diagnostics;
+using System.IO;
+using IWshRuntimeLibrary;
+
+
+namespace SimLauncher
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+
+        int numPrgms = 1;
+
+        List<string> prgmList = new List<string>();
+
+        MetroColorStyle colorStyle = Properties.Settings.Default.style;
+        MetroThemeStyle themeStyle = Properties.Settings.Default.theme;
+
+        Bitmap uploadImage;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.StyleManager = msm;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            appyThemeAndStyle();
+        }
+
+        private void appyThemeAndStyle()
+        {
+            msm.Theme = themeStyle;
+            msm.Style = colorStyle;
+            fillThemeandStyles();
+        }
+
+        private void fillThemeandStyles()
+        {
+            foreach (MetroColorStyle c in (MetroColorStyle[])Enum.GetValues(typeof(MetroColorStyle)))
+            {
+                styleBox.Items.Add(c.ToString());
+
+            }
+
+            foreach (MetroThemeStyle c in (MetroThemeStyle[])Enum.GetValues(typeof(MetroThemeStyle)))
+            {
+                themeBox.Items.Add(c.ToString());
+            }
+        }
+
+        private void themeBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (themeBox.SelectedIndex)
+            {
+
+                case 0:
+                    msm.Theme = MetroThemeStyle.Light;
+                    break;
+                case 1:
+                    msm.Theme = MetroThemeStyle.Dark;
+                    break;
+                default:
+                    msm.Theme = MetroThemeStyle.Default;
+                    break;
+            }
+            Properties.Settings.Default.theme = msm.Theme;
+        }
+
+        private void styleBox_SelectedValueChanged(object sender, EventArgs e)
+        {
+
+            msm.Style = (MetroColorStyle)styleBox.SelectedIndex;
+            Properties.Settings.Default.style = msm.Style;
+        }
+
+        private void addPrgm(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Choose Program...";
+            ofd.InitialDirectory = @"c:\";
+            ofd.Filter = "Exe Files (*.EXE)|*.exe|All files (*.exe)|*.exe";
+            ofd.FilterIndex = 2;
+            ofd.RestoreDirectory = true;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                MetroLabel lb = new MetroLabel();
+                lb.AutoSize = true;
+                lb.Text = $"{numPrgms}. {ofd.FileName}";
+                lb.Size = new System.Drawing.Size(550, 25);
+                lb.Name = $"prgm_{numPrgms}";
+                lb.Style = msm.Style;
+                lb.Theme = msm.Theme;
+
+                prgmList.Add($"{ofd.FileName}");
+                prgmPanel.Controls.Add(lb);
+            }
+            numPrgms++;
+        }
+
+
+
+        private void Form1_FormClosing(object sender, FormClosedEventArgs e)
+        {
+            Properties.Settings.Default.Save();
+        }
+
+        public static string ShowDialog(string text, string caption)
+        {
+            Form prompt = new Form()
+            {
+                Width = 500,
+                Height = 150,
+                FormBorderStyle = FormBorderStyle.FixedDialog,
+                Text = caption,
+                StartPosition = FormStartPosition.CenterScreen
+            };
+            MetroLabel textLabel = new MetroLabel() { Left = 50, Top = 20, Text = text, AutoSize = true };
+            MetroTextBox textBox = new MetroTextBox() { Left = 50, Top = 50, Width = 400 };
+            MetroButton confirmation = new MetroButton() { Text = "Ok", Left = 350, Width = 100, Top = 70, DialogResult = DialogResult.OK };
+            confirmation.Click += (sender, e) => { prompt.Close(); };
+            prompt.Controls.Add(textBox);
+            prompt.Controls.Add(confirmation);
+            prompt.Controls.Add(textLabel);
+            prompt.AcceptButton = confirmation;
+            
+            return prompt.ShowDialog() == DialogResult.OK ? textBox.Text : "";
+        }
+
+        private void addPrgmDelay(object sender, EventArgs e)
+        {
+            string delayLength = ShowDialog("Enter in milli-seconds how long you want the delay", "Delay");
+
+                MetroLabel lb = new MetroLabel();
+                lb.AutoSize = true;
+                lb.Text = $"{numPrgms}. Delay for {delayLength} ms";
+                lb.Size = new System.Drawing.Size(550, 25);
+                lb.Name = $"prgm_{numPrgms}";
+                lb.Style = msm.Style;
+                lb.Theme = msm.Theme;
+
+                prgmList.Add($"SLEEP {delayLength}");
+                prgmPanel.Controls.Add(lb);
+
+            numPrgms++;
+        }
+
+        private void createShortcut_Click(object sender, EventArgs e)
+        {
+            if (!System.IO.File.Exists($"{programNameTextBox.Text}.bat")) // If file does not exists
+            {
+                System.IO.File.Create($"{programNameTextBox.Text}.bat").Close(); // Create file
+                using (StreamWriter sw = System.IO.File.AppendText($"{programNameTextBox.Text}.bat"))
+                {
+                    sw.WriteLine("@echo off");
+                    foreach (var s in prgmList)
+                    {
+                        if (s.Contains("SLEEP"))
+                        {
+                            sw.WriteLine(s); // Write text to .txt file
+                        }else
+                        {
+                            sw.WriteLine($"start \"\" \"{s}\"");
+                        }
+                        
+                    }
+                }
+                string path = Path.Combine(Environment.CurrentDirectory, $"{programNameTextBox.Text}.bat");
+                CreateShortcut(programNameTextBox.Text, Environment.GetFolderPath(Environment.SpecialFolder.Desktop), path);
+            }
+        }
+
+        public static void CreateShortcut(string shortcutName, string shortcutPath, string targetFileLocation)
+        {
+            string shortcutLocation = System.IO.Path.Combine(shortcutPath, shortcutName + ".lnk");
+            WshShell shell = new WshShell();
+            IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutLocation);
+
+            shortcut.Description = "SimLauncher Shortcut";   // The description of the shortcut
+
+            shortcut.IconLocation = @"D:\temp\test.ico";           // The icon of the shortcut
+            shortcut.TargetPath = targetFileLocation;                 // The path of the file that will launch when the shortcut is run
+            shortcut.Save();                                    // Save the shortcut
+        }
+
+        private void addWebPage_Click(object sender, EventArgs e)
+        {
+            string s = ShowDialog("Enter Webpage", "Webpage");
+            MetroLabel lb = new MetroLabel();
+            lb.AutoSize = true;
+            lb.Text = $"{numPrgms}. Launch webpage {s}";
+            lb.Size = new System.Drawing.Size(550, 25);
+            lb.Name = $"prgm_{numPrgms}";
+            lb.Style = msm.Style;
+            lb.Theme = msm.Theme;
+
+            if (!s.Contains("http")) { prgmList.Add($"http://{s}"); }
+            else { prgmList.Add($"{s}"); }
+            prgmPanel.Controls.Add(lb);
+
+            numPrgms++;
+
+        }
+
+        private void imgConvert(Bitmap bmap)
+        {
+            using (FileStream FS = System.IO.File.OpenWrite(@"D:\temp\test.ico"))
+            {
+                Bitmap bitmap = bmap;
+                Icon.FromHandle(bitmap.GetHicon()).Save(FS);
+            }
+        }
+
+        private void iconAdd_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Choose Img...";
+            ofd.InitialDirectory = @"c:\";
+            ofd.Filter = "Png Files (*.PNG)|*.png|All files (*.png)|*.png";
+            ofd.FilterIndex = 2;
+            ofd.RestoreDirectory = true;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                imgConvert((Bitmap)Image.FromFile(ofd.FileName));
+            }
+            //imgConvert(uploadImage);
+        }
+    }
+}
